ac5e9c3c5e7039139661a6cba06cf9a4a79d9a1e
Matthew <49130090+mw-138@users.noreply.github.com>
2025-11-26 20:02:30 +0000

    feat: card

---
+++ app/components/card.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import * as React from "react";
+
+interface CardProps {
+  title: string;
+  description: string;
+  imageSrc: string;
+  stripColor: string;
+}
+
+const Card: React.FC<CardProps> = ({
+  title,
+  description,
+  imageSrc,
+  stripColor,
+}) => {
+  return (
+    <div
+      className="bg-white w-72 h-48 text-gray-500 rounded-md border-t-4 p-4 shadow-xl"
+      style={{ borderTopColor: stripColor }}
+    >
+      <h1 className="font-bold">{title}</h1>
+      <p>{description}</p>
+      <Image src={imageSrc} alt="" className="absolute bottom-5 right-5" />
+    </div>
+  );
+};
+
+export default Card;
